256f5a1961c27126226bf9cbf43ce60f18bb51c9
Eric Davenport <ericdavenport@pursuit.org>
2022-08-03 10:39:17 -0400

    initial commit-connecting local to repository

---
+++ BookClub/BookClub/BookClubApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookClubApp.swift
+//  BookClub
+//
+//  Created by Eric Davenport on 8/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BookClubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
